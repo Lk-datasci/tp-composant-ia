2383990231f2f721a423e657c13f41c09c1a3a8b
Lk-datasci <laura.kipiani@gmail.com>
2026-05-02 09:30:04 +0200

    fix

--- tp-composant-ia/scripts/docs_hooks.py
+++ tp-composant-ia/scripts/docs_hooks.py
@@ -1,26 +1,2 @@
-from pathlib import Path
-import shutil
-
-
-def update_readme_relative_links():
-    index = Path("docs/index.md")
-    if not index.exists():
-        return
-    with index.open() as f:
-        md = f.read()
-        updated_md = md.replace("./docs/", "./")
-        updated_md = updated_md.replace(
-            "(packages/",
-            "(https://github.com/loic/tp-composant-ia/tree/main/packages/",
-        )
-    with index.open("w") as f:
-        f.write(updated_md)
-
-
-def copy_readme():
-    shutil.copy("README.md", "docs/index.md")
-
-
 def pre_build(*args, **kwargs):
-    copy_readme()
-    update_readme_relative_links()
+    pass
